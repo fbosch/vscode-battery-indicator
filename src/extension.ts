d87561432d752a025d3aa6ff8b76ff9a378a04f4
Frederik Bosch <frede.bosch@gmail.com>
2019-05-14 14:19:13 +0200

    refactor: extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,179 +1,161 @@
 import * as execa from 'execa'
 import * as isCharging from 'is-charging'
-import * as linuxBattery from 'linux-battery'
-import * as osxBattery from 'osx-battery'
-import * as padEnd from 'pad-end'
 import * as powerInfo from 'node-power-info'
-import * as toDecimal from 'to-decimal'
 
-import { Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, commands, window } from 'vscode'
+import { Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, window } from 'vscode'
+
+const getUnixPowerInfo = () => new Promise(resolve => {
+	powerInfo.getChargeStatus(batteryStats => {
+		const [ stats ] = batteryStats
+		resolve(stats.powerLevel)
+	})
+})
+
+const getWindowsPowerInfo = () => new Promise((resolve, reject) => {
+	execa.stdout('WMIC', ['Path', 'Win32_Battery', 'Get', 'EstimatedChargeRemaining'])
+		.then(stdout => {
+			if (!stdout) reject(new Error('No battery could be found'))
+			const powerLevel = parseFloat(stdout.trim().split('\n')[1])
+			const normalizedPowerLevel = Math.round(powerLevel > 100 ? 100 : powerLevel) * 100
+			resolve(normalizedPowerLevel)
+		})
+})
+
 
 export function activate(context: ExtensionContext) {
-    console.log('🔋  battery-indicator is now active!')
 
-    const unix = () => new Promise(resolve => powerInfo.getChargeStatus(batteryStats => resolve(batteryStats[0].powerLevel)))
+	const batteryStateChecker =
+		process.platform === 'darwin' || process.platform === 'linux'
+			? getUnixPowerInfo
+			: getWindowsPowerInfo
 
-    const win = () => execa.stdout('WMIC', ['Path', 'Win32_Battery', 'Get', 'EstimatedChargeRemaining']).then(stdout => {
-        if (!stdout) {
-            return Promise.reject(new Error('No battery could be found'));
-        }
-        stdout = parseFloat(stdout.trim().split('\n')[1]);
-        return Math.round(toDecimal(stdout > 100 ? 100 : stdout) * 100)
-    })
+	const batteryIndicator = new BatteryIndicator(batteryStateChecker, isCharging)
+	const controller = new BatteryIndicatorController(batteryIndicator)
 
-    let batteryLevelStateChecker
-    if (process.platform === 'darwin' || process.platform === 'linux') {
-        batteryLevelStateChecker = unix
-    } else {
-        batteryLevelStateChecker = win
-    }
-
-    let batteryIndicator = new BatteryIndicator(batteryLevelStateChecker, isCharging)
-    let controller = new BatteryIndicatorController(batteryIndicator)
-
-    context.subscriptions.push(controller)
-    context.subscriptions.push(batteryIndicator)
+	context.subscriptions.push(controller)
+	context.subscriptions.push(batteryIndicator)
 }
 
 export class BatteryIndicatorController {
-    private _batteryIndicator: BatteryIndicator
-    private _disposable: Disposable
+	private batteryIndicator: BatteryIndicator
+	private disposable: Disposable
 
-    constructor(batteryIndicator: BatteryIndicator) {
-        this._batteryIndicator = batteryIndicator
-        this._batteryIndicator.updateStatus()
+	constructor(batteryIndicator: BatteryIndicator) {
+		let subscriptions: Disposable[] = []
+		batteryIndicator.updateStatus()
+		window.onDidChangeActiveTextEditor(batteryIndicator.updateStatus, subscriptions)
+		batteryIndicator.updateStatus()
+		this.disposable = Disposable.from(...subscriptions)
+	}
 
-        let subscriptions: Disposable[] = []
-        window.onDidChangeActiveTextEditor(this._onEvent, subscriptions)
-        window.onDidChangeActiveTextEditor(this._onEvent, subscriptions)
-
-        this._batteryIndicator.updateStatus()
-
-        this._disposable = Disposable.from(...subscriptions)
-    }
-
-    dispose() {
-        this._disposable.dispose()
-    }
-
-    private _onEvent() {
-        this._batteryIndicator.updateStatus()
-    }
-
+	dispose() {
+		this.disposable.dispose()
+	}
 }
 
 export class BatteryIndicator {
 
-    static POLLING_INTERVAL = 30000
-    static BATTERY_SECTION_FULL_SYMBOL = '|'
-    static BATTERY_SECTION_EMPTY_SYMBOL = ' '
-    static BAR_LENGTH = 10
-    static CHARGING_SYMBOL = '⚡'
-    static COLORS = {
-        full: '#8AE66C',
-        high: '#D8FA63',
-        medium: '#FED83A',
-        low: '#FD9943',
-        veryLow: '#FD5324'
-    }
 
-    private _battery
-    private _statusBarItem: StatusBarItem
-    private _pollingInterval
-    private _batteryPercentage
-    private _chargingState
+	private getChargingState: Function
+	private statusBarItem: StatusBarItem
+	private getBatteryState: Function
+	private polling: any
+	private pollingInterval: number
 
-    public interval
-    public getBatteryState
-    public getChargingState
-
-    constructor(getBatteryState, getChargingState) {
-        this.getBatteryState = getBatteryState
-        this.getChargingState = getChargingState
-        this.updateStatus()
-        this.pollingInterval = BatteryIndicator.POLLING_INTERVAL
-    }
-
-    set pollingInterval(pollingInterval) {
-        if (pollingInterval) {
-            pollingInterval = Math.max(pollingInterval, 1)
-            this._pollingInterval = pollingInterval
-            this.startPolling()
-        }
-    }
-
-    get pollingInterval() {
-        return this._pollingInterval
-    }
-
-    stopPolling() {
-        if (this.interval) {
-            clearInterval(this.interval)
-            this.interval = false
-        }
-    }
-
-    startPolling() {
-        if (this.interval) {
-            clearInterval(this.interval)
-        }
-        this.interval = setInterval(() => this.updateStatus(), this.pollingInterval)
-    }
-
-    updateStatus() {
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right)
-        }
-
-        let batteryLevel = this.getBatteryState().then(level => this._batteryPercentage = level),
-            chargingState = this.getChargingState().then(state => this._chargingState = state)
-
-        Promise.all([batteryLevel, chargingState]).then(() => {
-            this.updateStatusText(this._batteryPercentage, this._chargingState)
-            this._statusBarItem.color = this.getPowerColor(this._batteryPercentage)
-            this._statusBarItem.show()
-        }, () => {
-            console.warn('Current device does not have battery')
-            this.dispose()
-        })
-    }
-
-    getVisualIndicator(percentage, chargingState) {
-        let bar = BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(BatteryIndicator.BAR_LENGTH)
-        let currentLevel = padEnd(BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(Math.round((bar.length / 100) * percentage)), bar.length, BatteryIndicator.BATTERY_SECTION_EMPTY_SYMBOL)
-        return `[${currentLevel}] ${chargingState ? BatteryIndicator.CHARGING_SYMBOL : ''}`
-    }
-
-    updateStatusText(percentage, chargingState) {
-        if (percentage) {
-            this._statusBarItem.text = `${percentage}% ${this.getVisualIndicator(percentage, chargingState)}`
-        } else {
-            console.warn(`battery-indicator: invalid value: ${percentage}`)
-        }
-    }
-
-    getPowerColor(percentage) {
-        if (percentage === 100) {
-            return BatteryIndicator.COLORS.full
-        } else if (percentage >= 80) {
-            return BatteryIndicator.COLORS.high
-        } else if (percentage >= 50) {
-            return BatteryIndicator.COLORS.medium
-        } else if (percentage >= 30) {
-            return BatteryIndicator.COLORS.low
-        } else if (percentage <= 15) {
-            return BatteryIndicator.COLORS.veryLow
-        }
-        return null
-    }
-
-    dispose() {
-        this.stopPolling()
-        this._statusBarItem.dispose()
-    }
+	static pollingInterval = 30000
+	static batterySectionFullSymbol = '|'
+	static batterySectionEmptySymbol = '-'
+	static barLength = 10
+	static chargingSymbol = '⚡'
+	static colors = {
+		full: '#8AE66C',
+		high: '#D8FA63',
+		medium: '#FED83A',
+		low: '#FD9943',
+		veryLow: '#FD5324'
+	}
 
 
+	constructor(getBatteryState: Function, getChargingState: Function) {
+		this.getBatteryState = getBatteryState
+		this.getChargingState = getChargingState
+		this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right)
+		this.updateStatus()
+		this.pollingInterval = BatteryIndicator.pollingInterval
+	}
+
+	set interval(interval: number) {
+		if (interval) {
+			this.pollingInterval = Math.max(interval, 1)
+			this.startPolling()
+		}
+	}
+
+	get interval() {
+		return this.pollingInterval
+	}
+
+	get poll() {
+		return this.polling || false
+	}
+
+	updateStatus() {
+		if (!this.statusBarItem) {
+			this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right)
+		}
+		const readPowerLevel = this.getBatteryState()
+		const readChargingState = this.getChargingState()
+
+		Promise.all([readPowerLevel, readChargingState])
+			.then(batteryInformation => {
+				const [ powerLevel, isCharging ] = batteryInformation
+				this.updateStatusBarText(powerLevel, isCharging)
+			})
+			.catch(() => this.dispose())
+	}
+
+
+	getPowerColor(powerLevel: number) {
+		if (powerLevel === 100) return BatteryIndicator.colors.full
+		if (powerLevel >= 80) return BatteryIndicator.colors.high
+		if (powerLevel >= 50) return BatteryIndicator.colors.medium
+		if (powerLevel >= 30) return BatteryIndicator.colors.low
+		if (powerLevel <= 15) return BatteryIndicator.colors.veryLow
+		return null
+	}
+
+	getVisualIndicator(powerLevel: number, isCharging: boolean) {
+		const bar = BatteryIndicator.batterySectionFullSymbol.repeat(BatteryIndicator.barLength)
+		const powerLevelLength = Math.round((bar.length / 100) * powerLevel)
+		const powerLevelBar = BatteryIndicator.batterySectionFullSymbol.repeat(powerLevelLength)
+		const currentPowerLevelBar = powerLevelBar.padEnd(bar.length, BatteryIndicator.batterySectionEmptySymbol)
+		return `${currentPowerLevelBar} ${isCharging ? BatteryIndicator.chargingSymbol : ''}`
+	}
+
+	updateStatusBarText(powerLevel: number, isCharging: boolean) {
+		if (powerLevel) {
+			this.statusBarItem.text = `${powerLevel}% ${this.getVisualIndicator(powerLevel, isCharging)}`
+		}
+		this.statusBarItem.color = this.getPowerColor(powerLevel)
+		this.statusBarItem.show()
+	}
+
+	startPolling() {
+		this.stopPolling()
+		this.polling = setInterval(() => this.updateStatus(), this.pollingInterval)
+	}
+
+	stopPolling() {
+		if (this.polling) {
+			clearInterval(this.polling)
+			this.polling = false
+		}
+	}
+
+	dispose() {
+		this.stopPolling()
+		this.statusBarItem.dispose()
+	}
 }
 
-export function deactivate() {
-}
+export function  deactivate() { return null }
--- test/extension.test.ts
+++ test/extension.test.ts
@@ -30,40 +30,41 @@ suite("Battery Indicator", () => {
     })
 
     test('should stop polling and clear interval instance when the current device has no battery', () => {
-        assert(!batteryIndicatorOnDeviceWithNoBattery.interval)
+        assert(!batteryIndicatorOnDeviceWithNoBattery.poll)
     })
 
-    test('should show charging icon when power cable is plugged ind', () => {
-        assert(batteryIndicator.getVisualIndicator(100, true).includes(BatteryIndicator.CHARGING_SYMBOL))
-        assert(batteryIndicator.getVisualIndicator(80, true).includes(BatteryIndicator.CHARGING_SYMBOL))
+    test('should show charging icon when power cable is plugged in', () => {
+        console.log(batteryIndicator.getVisualIndicator(100, true))
+        assert(batteryIndicator.getVisualIndicator(100, true).includes(BatteryIndicator.chargingSymbol))
+        assert(batteryIndicator.getVisualIndicator(80, true).includes(BatteryIndicator.chargingSymbol))
     })
-   
+
    test('should represent percentage of battery (rounded up) as an ascii string', () => {
-        assert(batteryIndicator.getVisualIndicator(100, false).includes(BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(10)))
-        assert(batteryIndicator.getVisualIndicator(69, true).includes(BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(7)))
-        assert(batteryIndicator.getVisualIndicator(10, true).includes(BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(1)))
-        assert(batteryIndicator.getVisualIndicator(7, true).includes(BatteryIndicator.BATTERY_SECTION_FULL_SYMBOL.repeat(1)))
+        assert(batteryIndicator.getVisualIndicator(100, false).includes(BatteryIndicator.batterySectionFullSymbol.repeat(10)))
+        assert(batteryIndicator.getVisualIndicator(69, true).includes(BatteryIndicator.batterySectionFullSymbol.repeat(7)))
+        assert(batteryIndicator.getVisualIndicator(10, true).includes(BatteryIndicator.batterySectionFullSymbol.repeat(1)))
+        assert(batteryIndicator.getVisualIndicator(7, true).includes(BatteryIndicator.batterySectionFullSymbol.repeat(1)))
     })
 
     suite('Power Color', () => {
         test('should be dark green at 100% power', () => {
-            assert.equal(batteryIndicator.getPowerColor(100), BatteryIndicator.COLORS.full)
+            assert.equal(batteryIndicator.getPowerColor(100), BatteryIndicator.colors.full)
         })
         test('should be light green at 80% to 99% power', () => {
-            assert.equal(batteryIndicator.getPowerColor(80), BatteryIndicator.COLORS.high)
-            assert.equal(batteryIndicator.getPowerColor(90), BatteryIndicator.COLORS.high)
+            assert.equal(batteryIndicator.getPowerColor(80), BatteryIndicator.colors.high)
+            assert.equal(batteryIndicator.getPowerColor(90), BatteryIndicator.colors.high)
         })
         test('should be yellow at 50% to 79% power ', () => {
-            assert.equal(batteryIndicator.getPowerColor(50), BatteryIndicator.COLORS.medium)
-            assert.equal(batteryIndicator.getPowerColor(79), BatteryIndicator.COLORS.medium)
+            assert.equal(batteryIndicator.getPowerColor(50), BatteryIndicator.colors.medium)
+            assert.equal(batteryIndicator.getPowerColor(79), BatteryIndicator.colors.medium)
         })
         test('should be red at 30% to 49% power', () => {
-            assert.equal(batteryIndicator.getPowerColor(30), BatteryIndicator.COLORS.low)
-            assert.equal(batteryIndicator.getPowerColor(49), BatteryIndicator.COLORS.low)
+            assert.equal(batteryIndicator.getPowerColor(30), BatteryIndicator.colors.low)
+            assert.equal(batteryIndicator.getPowerColor(49), BatteryIndicator.colors.low)
         })
-        test('should be dark red below and equals 15% powe r', () => {
-            assert.equal(batteryIndicator.getPowerColor(1), BatteryIndicator.COLORS.veryLow)
-            assert.equal(batteryIndicator.getPowerColor(15), BatteryIndicator.COLORS.veryLow)
+        test('should be dark red below and equals 15% power', () => {
+            assert.equal(batteryIndicator.getPowerColor(1), BatteryIndicator.colors.veryLow)
+            assert.equal(batteryIndicator.getPowerColor(15), BatteryIndicator.colors.veryLow)
         })
     })
 })
